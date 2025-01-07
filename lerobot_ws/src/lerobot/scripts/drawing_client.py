0ac22817c1fd215750858d0ae20ba219cf57e6cf
godhj93 <godhj@unist.ac.kr>
2025-01-07 14:46:20 +0000

    update: integrated whisper and SDXL

--- lerobot_ws/src/lerobot/scripts/drawing_client.py
+++ lerobot_ws/src/lerobot/scripts/drawing_client.py
@@ -6,12 +6,15 @@ import numpy as np
 from lerobot.srv import DrawingRequest, DrawingCompleted, DrawingCompletedResponse
 from geometry_msgs.msg import Point
 from glob import glob
-import os
-import sys
+import os, sys, torch, tempfile, whisper, subprocess, pyaudio, wave
+from diffusers import AutoPipelineForText2Image
+from gtts import gTTS
+
 # Add the path to the transform module
 sys.path.append(os.path.join(os.path.dirname(__file__)))
-
 from utils.transform import transform_to_configuration_space
+import utils.sdxl_vectorize_to_stroke as stroke_func
+from utils.json_to_stroke import refine_stroke_and_to_csv
 
 def numpy_to_point_array(pts):
     """
@@ -90,8 +93,73 @@ def filter_out(data):
     t_pts = np.array(new_pts)
     
     return t_pts
+
+########## Whisper and SDXL ##########
+script_path = os.path.abspath(__file__)
+
+def speak(message):
+    with tempfile.NamedTemporaryFile(delete=True, suffix=".mp3") as temp_audio:
+        tts = gTTS(text=message, lang='en')
+        tts.save(temp_audio.name)
+        subprocess.run(
+            ["mpg123", temp_audio.name],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
+
+def record_audio(filename, duration=5, rate=16000, chunk=1024):
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paInt16,
+                    channels=1,
+                    rate=rate,
+                    input=True,
+                    frames_per_buffer=chunk)
+    frames = []
+
+    for _ in range(0, int(rate / chunk * duration)):
+        data = stream.read(chunk)
+        frames.append(data)
+
+    speak("Recording complete.")
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    with wave.open(filename, 'wb') as wf:
+        wf.setnchannels(1)
+        wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
+        wf.setframerate(rate)
+        wf.writeframes(b''.join(frames))
+
+def transcribe_audio(model, filename):
+    # model = whisper.load_model("base")
+    result = model.transcribe(filename, language="en")
+    return result['text']
+
+def confirm_with_voice(model: whisper.Whisper):
+    confirmation_file = "confirmation.wav"
+    
+    while not rospy.is_shutdown():
+        record_audio(confirmation_file, duration=3)
+        response = transcribe_audio(model, confirmation_file).strip().lower()
+        rospy.loginfo(f"Extracted text from voice: {response}")
+        
+        response = 'yes'
+        if "yes" in response:
+            speak("You said Yes.")
+            return True
+        elif "no" in response:
+            speak("You said No.")
+            return False
+        else:
+            speak("I did not understand. Please try again.")
+            rospy.loginfo("I did not understand. Please try again.")
+            
             
 if __name__ == "__main__":
+    
+    assert torch.cuda.is_available(), "CUDA is not available. Please install CUDA."
+    
     rospy.init_node("drawing_client")
     
     # Wait for the service to be available
@@ -99,6 +167,55 @@ if __name__ == "__main__":
     rospy.wait_for_service("drawing_request")
     rospy.loginfo("Waiting for the completed service to be available.")
     rospy.wait_for_service("drawing_completed")
+    
+    rospy.loginfo("Loading Whisper model...")
+    listener = whisper.load_model("base")
+    
+    rospy.loginfo("Loading Stable Diffusion model...")
+    pipeline_text2image = AutoPipelineForText2Image.from_pretrained(
+        "stabilityai/stable-diffusion-2-1-base",  #
+        torch_dtype=torch.float16,
+        variant="fp16",
+        use_safetensors=True
+    ).to("cuda")
+    
+    rospy.loginfo("Ready to listen.")
+    
+    audio_file = "input.wav"
+    filename = "image"
+    
+    while not rospy.is_shutdown():
+        
+        speak("Please record the name of the object you want to draw.")
+        rospy.loginfo("Please speak what you want to draw.")
+        
+        record_audio(audio_file, duration=5)
+        prompt = transcribe_audio(listener, audio_file).strip()
+        
+        prompt = "a cute cat" 
+        rospy.loginfo(f"Extracted text from voice: {prompt}")
+        speak(f"You said: {prompt}. Is this correct? Please say Yes or No.")
+        
+        if confirm_with_voice(listener):
+            speak("Thank you. Proceeding to the next step.")
+            break
+        else:
+            speak("Let's try again.")
+            rospy.loginfo("Recording again.")
+    
+    threshold_image, threshold_image_name = stroke_func.prompt_to_line_art_img(prompt, filename, pipeline_text2image)
+    stroke_list = stroke_func.img_to_svg_to_stroke(filename, threshold_image_name)
+    
+    output_dir = "drawing_data"
+    output_dir = os.path.join(os.path.dirname(script_path), output_dir)
+    
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir, exist_ok=True)
+        
+    csv_filename = f"refined_{filename}"
+    path_to_save = os.path.join(output_dir, csv_filename)
+    refine_stroke_and_to_csv(path_to_save, stroke_list=stroke_list)
+    rospy.loginfo(f"Stroke data saved to {path_to_save}.")
     
     try:
         # Create the service proxy
@@ -113,8 +230,6 @@ if __name__ == "__main__":
         # Process each file
         for file_path in csv_files:
             data = pd.read_csv(file_path)
-            
-            # pts = data.iloc[:, :3].values
             
             pts = filter_out(data)
 
